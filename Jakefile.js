e80045f163ab4c5aa6eedbdc84dd5fa76482d6aa
Fagin Vazio <vaziooizav@gmail.com>
2016-10-12 07:45:48 +1300

    Build script checks Node version

--- Jakefile.js
+++ Jakefile.js
@@ -1,10 +1,21 @@
 (function() {
   "use strict";
-  
+
+  var EXPECTED_NODE_VERSION = "v5.12.0";
+
   desc("Default build");  
-  task("default", function(){
+  task("default", [ "version" ], function(){
     console.log("\n\nBUILD OK");  
   });
+
+  desc("Check Node version");
+  task("version", function() {
+    console.log("Checking Node version: .");
     
+    var actualVersion = process.version;
+    if(actualVersion !== EXPECTED_NODE_VERSION) {
+      fail("Incorrect Node version: expected " + EXPECTED_NODE_VERSION + ", but was " + actualVersion);
+    }
+  });  
 }());
 
