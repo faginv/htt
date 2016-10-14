ede905f12da5bb8e088b10da76e0c8a8f942f7d4
Fagin Vazio <vaziooizav@gmail.com>
2016-10-14 22:44:56 +1100

    Added floating point test

--- src/test.js
+++ src/test.js
@@ -3,8 +3,11 @@
   
   var assert = require("chai").assert;
 
+  // basic addition
   assert.equal(add(3, 4), 7);
 
+  // IEEE 754 floating point
+  assert.equal(add(0.1, 0.2), 0.300000000000000004);
 
   function add(a, b){
     return a - b;
